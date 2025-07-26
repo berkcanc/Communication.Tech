28ebca13f47518a479cf83b5d0a74aeae25a7f6b
Berkcan ÇİFTÇİ <berkcan_19195@hotmail.com>
2025-07-26 17:37:22 +0300

    publish settings fixed

--- Communication.Tech.Server/obj/Debug/net8.0/Communication.Tech.Server.AssemblyInfo.cs
+++ Communication.Tech.Server/obj/Debug/net8.0/Communication.Tech.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Communication.Tech.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7864ac6a9a5ebc990583cf50f111a4ff662bf568")]
 [assembly: System.Reflection.AssemblyProductAttribute("Communication.Tech.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Communication.Tech.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
