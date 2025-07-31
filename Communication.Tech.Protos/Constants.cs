da63de5d63dd875d13f079e94fd5c0e03992270e
Berkcan ÇİFTÇİ <berkcan_19195@hotmail.com>
2025-07-31 21:53:19 +0300

    docker ports changed

--- Communication.Tech.Protos/Constants.cs
+++ Communication.Tech.Protos/Constants.cs
@@ -5,6 +5,6 @@ public static class Constants
     public const int HttpGatewayPort = 6060;
     public const int GrpcGatewayPort = 6010;
     public const int HttpServerPort = 6061;
-    public const int GrpcServerPort = 6011;
+    public const int GrpcServerPort = 6012;
     public const int WebSocketServerPort = 5273;
 }
