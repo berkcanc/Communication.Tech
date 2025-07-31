5fa38e12d18e60d28090682428a25c687b9318bc
Berkcan ÇİFTÇİ <berkcan_19195@hotmail.com>
2025-07-31 21:11:47 +0300

    gateway and server http port changed

--- Communication.Tech.Protos/Constants.cs
+++ Communication.Tech.Protos/Constants.cs
@@ -2,9 +2,9 @@ namespace Communication.Tech.Protos;
 
 public static class Constants
 {
-    public const int HttpGatewayPort = 5059;
+    public const int HttpGatewayPort = 6060;
     public const int GrpcGatewayPort = 6010;
-    public const int HttpServerPort = 5060;
+    public const int HttpServerPort = 6061;
     public const int GrpcServerPort = 6011;
     public const int WebSocketServerPort = 5273;
 }
