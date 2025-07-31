1d25425d70617d98582a4ac4c3568d09186609da
Berkcan ÇİFTÇİ <berkcan_19195@hotmail.com>
2025-07-31 21:56:05 +0300

    websocket port changed

--- Communication.Tech.Protos/Constants.cs
+++ Communication.Tech.Protos/Constants.cs
@@ -6,5 +6,5 @@ public static class Constants
     public const int GrpcGatewayPort = 6010;
     public const int HttpServerPort = 6061;
     public const int GrpcServerPort = 6012;
-    public const int WebSocketServerPort = 5273;
+    public const int WebSocketServerPort = 5274;
 }
