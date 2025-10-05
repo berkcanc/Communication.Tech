d567462edad22414e8ac2a7a8beac4339b20569f
Berkcan ÇİFTÇİ <berkcan_19195@hotmail.com>
2025-10-05 15:46:39 +0300

    response time measurement

--- Communication.Tech.Gateway/Program.cs
+++ Communication.Tech.Gateway/Program.cs
@@ -7,75 +7,59 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Prometheus;
 using StackExchange.Redis;
 using Constants = communication_tech.Constants;
+using System.Diagnostics;
 
 AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuration
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
 var configuration = builder.Configuration;
 
+// RabbitMQ
 builder.Services.Configure<RabbitMqSettings>(configuration.GetSection("RabbitMQ"));
 
+// Redis
 var redisConnectionString = configuration["Redis:ConnectionString"];
 builder.Services.AddSingleton<IConnectionMultiplexer>(sp => ConnectionMultiplexer.Connect(redisConnectionString));
 
+// Prometheus settings
 builder.Services.Configure<PrometheusSettings>(configuration.GetSection("Prometheus"));
 
+// Kestrel
 builder.WebHost.ConfigureKestrel(options =>
 {
-    // Global timeout ayarları
-    options.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(60);         // TCP bağlantısı açık kalma süresi
-    options.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(60);    // Header'ların alınması için bekleme süresi
-    options.Limits.MinRequestBodyDataRate = null;                        // Body veri hızı sınırlamasını kaldır
+    options.Limits.KeepAliveTimeout = TimeSpan.FromSeconds(60);
+    options.Limits.RequestHeadersTimeout = TimeSpan.FromSeconds(60);
+    options.Limits.MinRequestBodyDataRate = null;
     options.Limits.MinResponseDataRate = null;
-    
-    //Kafka, RabbitMQ, GraphQL Trigger, Redis
-    options.ListenAnyIP(Constants.HttpGatewayPort, o =>
-    {
-        o.Protocols = HttpProtocols.Http1AndHttp2;
-    });
-    
-    // gRPC
-    options.ListenAnyIP(Constants.GrpcGatewayPort, o =>
-    {
-        o.Protocols = HttpProtocols.Http1AndHttp2;
-    });
+
+    options.ListenAnyIP(Constants.HttpGatewayPort, o => o.Protocols = HttpProtocols.Http1AndHttp2);
+    options.ListenAnyIP(Constants.GrpcGatewayPort, o => o.Protocols = HttpProtocols.Http1AndHttp2);
 });
 
+// gRPC
+builder.Services.AddGrpc(options => options.Interceptors.Add<GrpcMetricsInterceptor>());
 
-
-
-
-builder.Services.AddGrpc(options =>
-{
-    options.Interceptors.Add<GrpcMetricsInterceptor>();
-});
+// HttpClient services
 builder.Services.AddHttpClient<HttpClientService>();
 builder.Services.AddSingleton<HttpClientService>();
 
-// Services
+// Other services
 builder.Services.AddSingleton<IPayloadGeneratorService, PayloadGeneratorService>();
-
-builder.Services.AddHttpClient();
-
 builder.Services.AddSingleton<IMetricsCollector<HttpMetric>, HttpMetricsCollector>();
 builder.Services.AddSingleton<IMetricsCollector<GrpcMetric>, GrpcMetricsCollector>();
 builder.Services.AddSingleton<IMetricsCollector<RedisMetric>, RedisMetricsCollector>();
 builder.Services.AddSingleton<IMetricsCollector<RabbitMqMetric>, RabbitMqMetricsCollector>();
-
-
 builder.Services.AddSingleton<IMetricsFileStorageService, MetricsFileStorageService>();
 builder.Services.AddSingleton<IPrometheusMetricService, PrometheusMetricService>();
 builder.Services.AddSingleton<KafkaProducerService>();
 builder.Services.AddSingleton<RabbitMQProducerService>();
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddScoped<GrpcMetricsInterceptor>();
 
 builder.Services.AddGrpcClient<Greeter.GreeterClient>(options =>
@@ -85,18 +69,45 @@ builder.Services.AddGrpcClient<Greeter.GreeterClient>(options =>
 
 builder.Services.AddGrpcReflection();
 
-
 var app = builder.Build();
 
+// Prometheus metric server
 app.UseMetricServer();
 
+// --- Bucket HTTP response time histogram ---
+var httpResponseHistogram = Metrics.CreateHistogram(
+    "http_request_duration_seconds",
+    "HTTP response time in seconds",
+    new HistogramConfiguration
+    {
+        Buckets =
+        [
+            0.001,0.002,0.004,0.008,0.016,0.032,0.064,0.128,0.256,0.512,1,2,4,8,16,32
+        ],
+        LabelNames = ["method", "status_code", "route"]
+    }
+);
+
+// Middleware for measuring response time
+app.Use(async (context, next) =>
+{
+    var sw = Stopwatch.StartNew();
+    await next();
+    sw.Stop();
+
+    httpResponseHistogram
+        .WithLabels(context.Request.Method, context.Response.StatusCode.ToString(), context.Request.Path)
+        .Observe(sw.Elapsed.TotalSeconds);
+});
+
+// --- Turnaround time middleware ---
 app.UseMiddleware<HttpMetricsMiddleware>();
 
+// gRPC, WebSockets
 app.UseHttpMetrics();
-
 app.UseWebSockets();
 
-
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -104,8 +115,7 @@ app.UseSwaggerUI(c =>
     c.RoutePrefix = "swagger";
 });
 
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
+// Routing
 app.MapControllers();
 
 app.Run();
